b6a5a4e9fb6a3312a78d66860fc2d959a067485b
Nicolò Loddo <61081917+nicoloddo@users.noreply.github.com>
2023-12-27 00:11:29 +0100

    Dont repeat extracted scales

--- MindmeldGame/scripts.js
+++ MindmeldGame/scripts.js
@@ -50,17 +50,31 @@ document.getElementById('guesser').addEventListener('touchmove', function(event)
 }, { passive: true });
 
 
+let usedLines = []; // Array to store used lines
+
 document.getElementById('extractScaleButton').addEventListener('click', function() {
-    // Assuming scales.txt is served from the server
     fetch('https://raw.githubusercontent.com/nicoloddo/nicoloddo.github.io/main/MindmeldGame/scale.txt')
     .then(response => response.text())
     .then(data => {
         const lines = data.split('\n');
-        const randomLine = lines[Math.floor(Math.random() * lines.length)];
-        const [leftWriting, rightWriting] = randomLine.split(':');
-        
-        document.getElementById('left-text').textContent = leftWriting;
-        document.getElementById('right-text').textContent = rightWriting;
+        let randomLine;
+        let attempts = 0;
+
+        do {
+            randomLine = lines[Math.floor(Math.random() * lines.length)];
+            attempts++;
+            // Prevent infinite loop if all lines have been used
+            if (attempts > lines.length) break;
+        } while (usedLines.includes(randomLine));
+
+        if (!usedLines.includes(randomLine)) {
+            const [leftWriting, rightWriting] = randomLine.split(':');
+            document.getElementById('left-text').textContent = leftWriting;
+            document.getElementById('right-text').textContent = rightWriting;
+            usedLines.push(randomLine); // Add the used line to the array
+        } else {
+            console.log("All lines have been used.");
+        }
     })
     .catch(error => {
         console.error('Error fetching the scales.txt file:', error);
