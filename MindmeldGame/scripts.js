60e28a304ad1b52306e217112b2fd4c198a74d37
nicoloddo <gnicoloddo@gmail.com>
2023-12-26 21:45:00 +0100

    Better arrow and implemented guesser

--- MindmeldGame/scripts.js
+++ MindmeldGame/scripts.js
@@ -14,3 +14,37 @@ document.getElementById('lidButton').addEventListener('click', function() {
         lid.classList.add('lid-visible');
     }
 });
+
+var guesserRotation = 0;
+var initialTouchX;
+
+function rotateGuesser(deltaX) {
+    var rotationStep = 1; // Adjust the sensitivity of rotation
+    var tempRotation = guesserRotation + deltaX * rotationStep;
+    if(tempRotation <= 90 && guesserRotation >= 0) {
+        guesserRotation += deltaX * rotationStep;
+        document.getElementById('guesser').style.transform = 'rotate(' + guesserRotation + 'deg)';
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    var deltaX = 0;
+    if (event.key === 'ArrowRight' || event.key === 'ArrowUp') {
+        deltaX = 1;
+    } else if (event.key === 'ArrowLeft' || event.key === 'ArrowDown') {
+        deltaX = -1;
+    }
+    rotateGuesser(deltaX);
+});
+
+// Touch event listeners
+document.getElementById('guesser').addEventListener('touchstart', function(event) {
+    initialTouchX = event.touches[0].clientX;
+}, { passive: true });
+
+document.getElementById('guesser').addEventListener('touchmove', function(event) {
+    var currentTouchX = event.touches[0].clientX;
+    var deltaX = currentTouchX - initialTouchX;
+    rotateGuesser(deltaX);
+    initialTouchX = currentTouchX; // Update initial touch position
+}, { passive: true });
